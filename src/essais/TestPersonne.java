46122709ee2e02296a9c9d1fc588c7ce2f54e970
Amine KERKOUB <kerk.amine@gmail.com>
2020-06-25 13:11:31 +0200

    V03

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -4,14 +4,16 @@ import entites.Personne;
 import entites.AdressePostale;
 
 public class TestPersonne {
-
-	public static void main(String[] args) {
 	
+	public static void main(String[] args) {
+		
+		
+		
+		
 		Personne pers1 = new Personne();
-		
-		
 		AdressePostale adresse1 = new AdressePostale ();
 		pers1.adresse=adresse1;
+		
 
 		Personne pers2 = new Personne ();
 		AdressePostale adresse2 = new AdressePostale ();
