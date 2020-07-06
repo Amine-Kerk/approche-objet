1ffd3ba1afd336aac54a4b1aa7c2f8efe81c5b4f
Amine KERKOUB <kerk.amine@gmail.com>
2020-07-06 11:47:11 +0200

    TP guidé lecture d'un fichier

---
+++ src/essais/Cercle.java
@@ -0,0 +1,27 @@
+package essais;
+
+public class Cercle   {
+
+	private double ray;
+	
+	
+//constructeur avec un seul paramétre
+	public Cercle(double ray) {
+		
+		this.ray = ray;
+	}
+
+//get et set de notre variable	
+	public double getRay() {
+		return ray;
+	}
+
+	public void setRay(double ray) {
+		this.ray = ray;
+	}
+
+// les methodes de calcule et affichage du resultat 
+	
+	
+	
+}
